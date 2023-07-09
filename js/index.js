755ae9236a7c7e08f8d8102e79f3731efbc21d1f
Maryna Verbovska <verbovskaya.freelance@gmail.com>
2023-07-09 14:26:15 +0300

    feat: add code example

---
+++ js/index.js
@@ -0,0 +1,50 @@
+//accordion
+function accordion() {let acc = document.querySelectorAll('.accordion__item');
+
+for (let i = 0; i < acc.length; i++){
+   acc[i].addEventListener('click', function () {
+      this.classList.toggle('active');
+      let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } 
+   })
+} 
+}
+accordion(); 
+//portfolio
+const slider = document.querySelector('.slider');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+const slides = Array.from(slider.querySelectorAll('img'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+// Функция для показа предыдущего слайда
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+// Функция для показа следующего слайда
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+// Функция для обновления отображения слайдера
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+}
+
+// Инициализация слайдера
+updateSlider();
